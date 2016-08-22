faaf6f327b8754a6a44bfbb4e5f6c121620b3c75
Cryptogenic <cryptogenicdev@gmail.com>
2016-08-21 22:32:04 -0400

    Version 1.0
    + Added ability to dump modules
    + Added ability to get the WebKit process' PID
    + Added ability to get module information
    - Removed redundant files
    
    See notes in README.md for important notes, especially regarding dumping WebKit2

--- ps4console.py
+++ ps4console.py
@@ -13,7 +13,7 @@ import sys
 
 DEVNULL = open(os.devnull, 'wb')
 
-exploitCmds = {'runpoc', 'dump', 'getmodules', 'getpid'}
+exploitCmds = {'runpoc', 'dump', 'getmodules', 'getpid', 'getsysinfo'}
 exploitCmd  = ''
 exploitPage = ''
 
@@ -48,16 +48,18 @@ def runConsoleInterpretter():
 			print("\r")
 			print("[ PS4Console Version 1.1 - Help Center ]")
 			print("\r")
-			print("List of available commands:")
+			print("Commands marked with a '!!!' are currently not functional.")
+			print("\r\r")
+			print("List of commands:")
 			print("-")
 			print("authors")
 			print("clear")
-			print("dump [module ID] [file name]")
-			print("getaddress {vtable/webkit/[module ID]}")
+			print("dump")
+			print("!!! getaddress {vtable/webkit/[module ID]}")
 			print("getmodules")
 			print("getpid")
-			print("getsysinfo")
-			print("getversion {webkit/firmware}")
+			print("!!! getsysinfo")
+			print("!!! getversion {webkit/firmware}")
 			print("help")
 			print("runpoc")
 			print("shutdown (server, not system)")
@@ -81,16 +83,65 @@ class PS4Console(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.end_headers()
 			path = self.path[1:]
 			self.wfile.write(open(path).read())
+		elif 'moduleToDump.txt' in self.path:
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			self.wfile.write(open("moduleToDump.txt").read())
 		else:
 			self.send_response(200);
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
 
-			if runConsoleInterpretter() == 'runpoc':
+			cmdToRun = runConsoleInterpretter()
+
+			if cmdToRun == 'runpoc':
 				print("\r")
 				print("[ PS4Console Version 1.1 - POC Test ]")
 				print("\r")
-				self.wfile.write(open('runpoc.html').read())
+				self.wfile.write(open('pages/runpoc.html').read())
+
+			if cmdToRun == 'getmodules':
+				print("\r")
+				print("[ PS4Console Version 1.1 - Module Information ]")
+				print("\r")
+				self.wfile.write(open('pages/sysinfo.html').read())
+
+			if cmdToRun == 'getpid':
+				print("\r")
+				print "One of the two WebKit PID's is:",
+				self.wfile.write(open('pages/getpid.html').read())
+
+			# Here we will have sub-input for parameters
+			if cmdToRun == 'dump':
+				print("\r")
+				print("[ PS4Console Version 1.1 - Module Dumper ]")
+				print("\r")
+
+				print "Enter the name or ID (including 0x prefix) of the module you wish to dump: ",
+
+				nameModule = raw_input()
+
+				if nameModule == 'libSceSysmodule' or nameModule == '0xc':
+					self.wfile.write(open('pages/dumping/dump-sysmodule.html').read())
+				elif nameModule == 'libSceNetCtl' or nameModule == '0x1b':
+					self.wfile.write(open('pages/dumping/dump-netctl.html').read())
+				elif nameModule == 'libSceRegMgr' or nameModule == '0x1f':
+					self.wfile.write(open('pages/dumping/dump-regmgr.html').read())
+				elif nameModule == 'libSceRtc' or nameModule == '0x20':
+					self.wfile.write(open('pages/dumping/dump-scertc.html').read())
+				elif nameModule == 'libScePad' or nameModule == '0x21':
+					self.wfile.write(open('pages/dumping/dump-scepad.html').read())
+				elif nameModule == 'libSceOrbisCompat' or nameModule == '0x23':
+					self.wfile.write(open('pages/dumping/dump-orbis.html').read())
+				elif nameModule == 'libSceSysCore' or nameModule == '0x25':
+					self.wfile.write(open('pages/dumping/dump-syscore.html').read())
+				elif nameModule == 'libSceSystemService' or nameModule == '0x26':
+					self.wfile.write(open('pages/dumping/dump-sysserv.html').read())
+				elif nameModule == 'libSceSsl' or nameModule == '0x35':
+					self.wfile.write(open('pages/dumping/dump-scessl.html').read())
+				else:
+					print "You are attempting to dump an unknown or unsupported module..."
 
 	def do_POST(self):
 		if '/debug/log' in self.path:
@@ -98,6 +149,18 @@ class PS4Console(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.send_response(200)
 			self.end_headers()
 			print data_string
+
+		if '/debug/bin' in self.path:
+			filename = self.path.split("/")[-1]
+
+			dataString = self.rfile.read(int(self.headers['Content-length']))
+			self.send_response(200)
+			self.end_headers()
+			f = open('Dumps/Modules/' + filename, mode='wb')
+			f.write(dataString)
+			f.close()
+
+			print 'Module has been dumped to %s' % filename
 
 	def log_message(self, format, *args):
 		return
